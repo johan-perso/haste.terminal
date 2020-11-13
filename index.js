d48b461f15f365f418646cfeea118177aed10c40
Johan <41506568+anticoupable@users.noreply.github.com>
2020-11-13 18:50:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// Dépendances
+const term = require('terminal-kit').terminal; // Utilisation de terminal-kit : Demande de texte, Demande d'appuie de touches, Envoie de message dans la console en couleur
+const hastebin = require('hastebin.js'); // Utilisation de hastebin : Création de haste
+const haste = new hastebin({ /* url: 'hastebin.com */ }); // Utilisation de hastebin : Création de haste
+const config = require('./config.json'); // Utilisation de config.json : Traduction
+
+
+
+// Code principal
+term.white(config.textInput); // Demande de texte
+term.inputField(
+    function( error , input ){
+        
+        // Si aucun texte
+        if(!input){
+             console.log("\n" + config.writeThing);
+             return process.exit();
+        }
+        
+        // Crée le lien puis l'envoyer dans la console
+        const link = haste.post(input, 'md').then(link => {
+            term.white("\n" + config.linkGenerated); term.cyan(link); // Envoie du lien
+            
+            // Demande pour voir le texte
+            function question() {
+	term("\n" + config.viewText);
+	term.yesOrNo( { yes: config.touchY , no: config.touchN } , function( error , result ) {
+	
+		if (result) {
+			term.cyan("\n" + input + "\n") ;
+			process.exit();
+		}
+		else {
+			term.white("\n" + config.shutdown + "\n");
+            process.exit();
+		}
+	} );
+}
+question();
+            
+        });
+    }
+);
+
+
